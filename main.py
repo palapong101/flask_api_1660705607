84821eefa660cbd833abb839a5928d813b1e4adf
Palapong Takamho <palapong.taka@bumail.net>
2025-02-27 21:16:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, BU 1660705607'
+    return 'Hello, BU Palapong'
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
